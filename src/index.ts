3106e0d21c87ad1e0f450576e8dc4f48053e9438
MaryAnzh <maryang@mail.ru>
2022-11-24 23:32:01 +0300

    feat: add webpack

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+"use strict";
+import './assets/style/style.scss';
+
+import { LocalStorageService } from './service/localStorage';
+
+const body = document.querySelector('body');
+
+// const page = () => new MainPage(body);
+// window.onload = page;
---
+++ src/view/control.ts
@@ -0,0 +1,30 @@
+
+interface IAtr {
+    name: string,
+    value: string | boolean,
+}
+export class Control {
+    public node;
+
+    constructor(perentNode: HTMLElement, tagName: string = 'div', className: string | string[], content: string = '', atr: IAtr[] | null) {
+        const element = document.createElement(tagName);
+        if (Array.isArray(className)) {
+            className.forEach(name => element.classList.add(name));
+        }
+        if (typeof className === 'string') {
+            element.className = className;
+        }
+        element.textContent = content;
+        if (perentNode) {
+            perentNode.append(element);
+        }
+        if (atr) {
+            atr.forEach(el => element.setAttribute(el.name, el.value));
+        }
+        this.node = element;
+    }
+
+    destroy() {
+        this.node.remove();
+    }
+}
