f9c9eaea121c5f94d5336aafa9c9b6fd31ccdbc7
MaryAnzh <maryang@mail.ru>
2022-11-30 12:40:01 +0300

    feat: controls elements layout

--- src/view/components/audio-player/audio-player.ts
+++ src/view/components/audio-player/audio-player.ts
@@ -3,11 +3,17 @@ import { Control } from '../../../service/control';
 export class AudioPlayerCustomHTML extends HTMLElement {
     private audio = new Audio();
     private container: Control;
+
     private controls: Control;
+
     private playButton: Control;
     private timeBar: Control;
-    private timeBarTimer: Control;
-    private timeBarAudioTime: Control;
+    private timeBarRunner: Control;
+
+    private times: Control;
+    private timesTimer: Control;
+    private timesAudioTime: Control;
+
     private soundBar: Control;
     private playList: Control;
     isPlay = false;
@@ -35,7 +41,14 @@ export class AudioPlayerCustomHTML extends HTMLElement {
         this.controls = new Control(this.container.node, 'div', 'audio-player__controls');
         this.playButton = new Control(this.controls.node, 'button', 'audio-player__controls__button', '+');
         this.playButton.node.onclick = () => this.play();
+
         this.timeBar = new Control(this.controls.node, 'div', 'audio-player__controls__time-bar');
+        this.timeBarRunner = new Control(this.timeBar.node, 'div', 'audio-player__controls__time-bar__runner');
+
+        this.times = new Control(this.controls.node, 'div', 'audio-player__controls__times');
+        this.timesTimer = new Control(this.times.node, 'p', 'audio-player__controls__times__timer', '00:00');
+        this.timesAudioTime = new Control(this.times.node, 'p', 'audio-player__controls__times__audio-time', '00:00');
+
         this.soundBar = new Control(this.controls.node, 'div', 'audio-player__controls__sound-bar');
         this.playList = new Control(this.container.node, 'div', 'audio-player__play-list');
     }
