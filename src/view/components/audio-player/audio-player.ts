95190c3d3b2d1e63096e0531f66834c0a8d9627a
MaryAnzh <maryang@mail.ru>
2022-11-30 16:47:26 +0300

    feat: add play list layout

--- src/view/components/audio-player/audio-player.ts
+++ src/view/components/audio-player/audio-player.ts
@@ -83,12 +83,18 @@ export class AudioPlayerCustomHTML extends HTMLElement {
         const ul = new Control(perent, 'ul', 'items-list');
         items.forEach(item => {
             const li = new Control(ul.node, 'li', 'items-list__item');
+            const audio = new Audio();
+            audio.src = item.src;
             const svg = document.createElementNS(`http://www.w3.org/2000/svg`, "svg");
             svg.setAttribute('viewBox', '0 0 30 15.7');
             svg.innerHTML = `<path d="M29.7,1.4l-14,14c-0.4,0.4-1,0.4-1.4,0l-14-14c-0.4-0.4-0.4-1,0-1.4h29.4C30.1,0.4,30.1,1,29.7,1.4z"/>
             `;
             li.node.appendChild(svg);
-            const text = new Control(li.node, 'p', '', item.title);
+            const itemInfo = new Control(li.node, 'div', 'items-list__item__info');
+            const name = new Control(itemInfo.node, 'p', '', item.title);
+            const time = new Control(itemInfo.node, 'p', '');
+            audio.addEventListener('loadedmetadata', () => time.node.textContent = this.viewTime(audio.duration));
+
         });
     }
 
