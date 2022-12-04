f5926a46de353312a0c191bb328eeff9c9170593
MaryAnzh <maryang@mail.ru>
2022-12-04 12:17:52 +0300

    feat: add list stile

--- src/view/components/audio-player/play-list-item/play-list-item.ts
+++ src/view/components/audio-player/play-list-item/play-list-item.ts
@@ -14,12 +14,12 @@ export class PlayItem extends Control {
     constructor(item: IPlayItem) {
         super(null, 'li', 'items-list__item');
 
-        this.playIcon = new Control(this.node, 'div', ['items-list__item__icon', 'not-active-atem']);
-        const svg2 = document.createElementNS(`http://www.w3.org/2000/svg`, "svg");
-        svg2.setAttribute('viewBox', '0 0 30 15.7');
-        svg2.innerHTML = `<path d="M29.7,1.4l-14,14c-0.4,0.4-1,0.4-1.4,0l-14-14c-0.4-0.4-0.4-1,0-1.4h29.4C30.1,0.4,30.1,1,29.7,1.4z"/>
+        this.playIcon = new Control(this.node, 'div', ['items-list__item__icon', 'not-active-item']);
+        const playSVG = document.createElementNS(`http://www.w3.org/2000/svg`, "svg");
+        playSVG.setAttribute('viewBox', '0 0 30 15.7');
+        playSVG.innerHTML = `<path d="M29.7,1.4l-14,14c-0.4,0.4-1,0.4-1.4,0l-14-14c-0.4-0.4-0.4-1,0-1.4h29.4C30.1,0.4,30.1,1,29.7,1.4z"/>
         `;
-        this.playIcon.node.appendChild(svg2);
+        this.playIcon.node.appendChild(playSVG);
         this.playIcon.node.onclick = () => this.onClick();
 
         this.container = new Control(this.node, 'div', 'items-list__item__info');
@@ -29,11 +29,11 @@ export class PlayItem extends Control {
         this.time = new Control(this.container.node, 'p', '', timeView);
 
         this.arroyIcon = new Control(this.node, 'div', ['items-list__item__icon', 'arrow-to-right']);
-        const svg = document.createElementNS(`http://www.w3.org/2000/svg`, "svg");
-        svg.setAttribute('viewBox', '0 0 30 15.7');
-        svg.innerHTML = `<path d="M29.7,1.4l-14,14c-0.4,0.4-1,0.4-1.4,0l-14-14c-0.4-0.4-0.4-1,0-1.4h29.4C30.1,0.4,30.1,1,29.7,1.4z"/>
+        const arrowSVG = document.createElementNS(`http://www.w3.org/2000/svg`, "svg");
+        arrowSVG.setAttribute('viewBox', '0 0 30 15.7');
+        arrowSVG.innerHTML = `<path d="M29.7,1.4l-14,14c-0.4,0.4-1,0.4-1.4,0l-14-14c-0.4-0.4-0.4-1,0-1.4h29.4C30.1,0.4,30.1,1,29.7,1.4z"/>
         `;
-        this.arroyIcon.node.appendChild(svg);
+        this.arroyIcon.node.appendChild(arrowSVG);
         this.arroyIcon.node.onclick = () => this.onClick();
     }
 
